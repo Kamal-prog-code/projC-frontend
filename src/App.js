92baf4be1454934a30826348e439b6daba6ee2fb
Kamal-prog-code <karrerajisyam123@gmail.com>
2022-10-20 11:50:29 +0530

    update UI Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React,{useState} from 'react'
 import './App.css'
 
-
 const App = () => {
-  
   const [data,setData] = useState({
     email : '',
     password :'',
@@ -40,46 +38,48 @@ const App = () => {
   return (
 
         <div className='modal'>
-          <center>
-          <h1 className='main-title'>
-          </h1>
-        </center>
-        <form className='modal-content' onSubmit={submitHandler}>
-          <h3>Sign In</h3><br/>
-          <div>
-          <p className='dont-have'>Don't have an account? <a href='#'>Sign Up</a></p>
-          <br/>
-          </div>
+          <center><h1 className='main-title'></h1></center>
+          <div className='splitScreen'>
+            <img alt='logo' style={{ width: "50%" }} className='responsive' src="idea_connect.png" />
+            <form className='modal-content' style={{ width: "50%" }} onSubmit={submitHandler}>
+              <h3>Sign In</h3><br/>
+              <div>
+              <p className='dont-have'>Don't have an account? <a href='#'>Sign Up</a></p>
+              <br/>
+              </div>
 
-          <div>
-            <label>Email Address/Mobile Number</label><br/>
-            <input className='input-feild input_form' type='text' name='email'
-             placeholder='Enter Email ID/Mobile Number'
-             onChange={changeHanddler}
-             value={email}/><br/><br/><br/>
+              <div>
+                <label>Email Address/Mobile Number</label><br/>
+                <input className='input-feild input_form' type='text' name='email'
+                placeholder='Enter Email ID/Mobile Number'
+                onChange={changeHanddler}
+                value={email}/><br/><br/><br/>
+              </div>
+              
+              <div>
+                <label>Password</label><br/>
+                <input class="input_form" type='password' name='password'
+                placeholder='Enter Password'
+                value={password}
+                onChange={changeHanddler}/><br/><br/>
+              </div>
+              <div className='remenber-forgot'>
+              <label>
+              <input type="checkbox" name="remember"/> Remember me
+              </label><br/>
+                <a href='#' style={{ marginRight: 0 }}>Forgot password</a><br/><br/>
+              </div>
+              <center>
+                <button  className='sign-btn'>Sign In</button><br/><br/>
+                <a href='#'>Login with OTP</a><br/><br/><br/><hr/>
+              </center>
+              <center>
+                <p>Idea Connect. All rights reserved.</p>
+                <a href='#'>Privacy Policy</a>
+                </center>
+            </form>
           </div>
-          
-          <div>
-            <label>Password</label><br/>
-            <input class="input_form" type='password' name='password'
-             placeholder='Enter Password'
-             value={password}
-             onChange={changeHanddler}/><br/><br/>
-          </div>
-          <div className='remenber-forgot'>
-          <label>
-          <input type="checkbox" name="remember"/> Remember me
-          </label><br/>
-            <a href='#'>Forgot password</a><br/><br/>
-          </div>
-            <button  className='sign-btn'>Sign In</button><br/><br/>
-            <a href='#'>Login with OTP</a><br/><br/><br/><hr/>
-            <p>Kredily.All rights reserved.</p>
-            <a href='#'>Privacy Policy</a>
-        </form>
-        </div>
-        
-     
+        </div>     
   )
 }
 
